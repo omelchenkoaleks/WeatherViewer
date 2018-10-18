3071c178938505a7de7aecb11f15783297571fda
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-18 17:07:38 +0300

    refactoring

--- app/src/main/java/com/omelchenkoaleks/weatherviewer/WeatherArrayAdapter.java
+++ app/src/main/java/com/omelchenkoaleks/weatherviewer/WeatherArrayAdapter.java
@@ -4,8 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -47,16 +45,11 @@ public class WeatherArrayAdapter extends ArrayAdapter<Weather> {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView =
                     inflater.inflate(R.layout.list_item, parent, false);
-            viewHolder.conditionImageView =
-                    (ImageView) convertView.findViewById(R.id.conditionImageView);
-            viewHolder.dayTextView =
-                    (TextView) convertView.findViewById(R.id.dayTextView);
-            viewHolder.lowTextView =
-                    (TextView) convertView.findViewById(R.id.lowTextView);
-            viewHolder.hiTextView =
-                    (TextView) convertView.findViewById(R.id.hiTextView);
-            viewHolder.humidityTextView =
-                    (TextView) convertView.findViewById(R.id.humidityTextView);
+            viewHolder.conditionImageView = convertView.findViewById(R.id.conditionImageView);
+            viewHolder.dayTextView = convertView.findViewById(R.id.dayTextView);
+            viewHolder.lowTextView = convertView.findViewById(R.id.lowTextView);
+            viewHolder.hiTextView = convertView.findViewById(R.id.hiTextView);
+            viewHolder.humidityTextView = convertView.findViewById(R.id.humidityTextView);
             convertView.setTag(viewHolder);
         }
         else {
@@ -104,7 +97,7 @@ public class WeatherArrayAdapter extends ArrayAdapter<Weather> {
 
                 try (InputStream inputStream = connection.getInputStream()) {
                     bitmap = BitmapFactory.decodeStream(inputStream);
-                    bitmaps.put(params[0], bitmap); // cache for later use
+                    bitmaps.put(params[0], bitmap);
                 }
                 catch (Exception e) {
                     e.printStackTrace();
